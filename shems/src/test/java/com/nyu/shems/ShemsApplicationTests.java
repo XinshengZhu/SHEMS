443debc8b89033eaa23c04c4ab771bb9e1dbdbaa
XinshengZhu <142745727+XinshengZhu@users.noreply.github.com>
2023-12-15 14:13:29 -0500

    Update ShemsApplicationTests.java

--- shems/src/test/java/com/nyu/shems/ShemsApplicationTests.java
+++ shems/src/test/java/com/nyu/shems/ShemsApplicationTests.java
@@ -1,15 +1,7 @@
 package com.nyu.shems;
 
-import com.nyu.shems.entity.Servicelocation;
-import com.nyu.shems.service.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import javax.annotation.Resource;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @SpringBootTest
 class ShemsApplicationTests {
